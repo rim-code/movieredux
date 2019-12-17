d80f9cac70ae728258ac8a102717f722669f038a
rim-code <friouirim@gmail.com>
2019-12-17 13:49:13 +0100

    Updates

---
+++ precache-manifest.17a96f8b335bfa59e80c0a1d8d45b0a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbf64dcc1159e38bbaf3209a9245907",
+    "url": "/movieredux/index.html"
+  },
+  {
+    "revision": "a31b2a032faa75bac51c",
+    "url": "/movieredux/static/css/main.3ba39aa0.chunk.css"
+  },
+  {
+    "revision": "f6de9c86568592901a02",
+    "url": "/movieredux/static/js/2.956cc6d8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movieredux/static/js/2.956cc6d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a31b2a032faa75bac51c",
+    "url": "/movieredux/static/js/main.e1c3f3f8.chunk.js"
+  },
+  {
+    "revision": "dad9ec6a8fcf7a9a8438",
+    "url": "/movieredux/static/js/runtime-main.d32176e4.js"
+  }
+]);
